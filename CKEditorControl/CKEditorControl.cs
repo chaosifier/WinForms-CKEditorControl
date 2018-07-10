a82d0e598ea33754b8ede564524cc048361b8840
Sagar Dahal <sagar.dahal@braindigit.com>
2018-07-10 20:58:12 +0545

    Created CKEditorControl with sample project.

---
+++ CKEditorControl/CKEditorControl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace CKEditorBrowserControls
+{
+    [ComVisible(true)]
+    public partial class CKEditorControl : WebBrowser
+    {
+        private Uri _ckEditorIndexPath;
+        public CKEditorControl(Uri ckEditorIndexPath)
+        {
+            _ckEditorIndexPath = ckEditorIndexPath;
+
+            Url = ckEditorIndexPath;
+            ScriptErrorsSuppressed = true;
+            IsWebBrowserContextMenuEnabled = false;
+            AllowWebBrowserDrop = false;
+            ObjectForScripting = this;
+            WebBrowserShortcutsEnabled = false;
+        }
+
+        public void SetContent(string content)
+        {
+            Document.InvokeScript("setContent",
+               new String[] { content });
+        }
+
+        public string GetContent()
+        {
+            return Document.InvokeScript("getContent").ToString();
+        }
+    }
+}
---
+++ CkEditorControlSample/Form1.cs
@@ -0,0 +1,54 @@
+﻿using CKEditorBrowserControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CkEditorControlSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        { 
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var tabCtrl = new TabControl()
+            {
+                Dock = DockStyle.Fill
+            };
+
+            var curDir = Directory.GetCurrentDirectory();
+            var ckEditorUri = new Uri(String.Format("file:///{0}/Resources/CkEditor/index.html", curDir));
+
+            for (int i = 0; i < 8; i++)
+            {
+                var tabPg = new TabPage($"Tab {i}");
+                var ckEditorCtrl = new CKEditorControl(ckEditorUri)
+                {
+                    Dock = DockStyle.Fill
+                };
+
+                tabPg.Controls.Add(ckEditorCtrl);
+
+                int indx = i;
+                ckEditorCtrl.DocumentCompleted+= (sss,ee)=> {
+                    ckEditorCtrl.SetContent($"Tab {indx} Content");
+                };
+
+                tabCtrl.Controls.Add(tabPg);
+            }
+
+            Controls.Add(tabCtrl);
+        }
+    }
+}
